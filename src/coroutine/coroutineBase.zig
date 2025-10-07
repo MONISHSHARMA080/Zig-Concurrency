401726b4fe4b2125a0cfa90b088e05bb7011690c
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-10-07 11:57:48 +0530

    implementing new yield and resume

--- src/coroutine/coroutineBase.zig
+++ src/coroutine/coroutineBase.zig
@@ -39,14 +39,22 @@ const archInfo: ArchInfo = switch (builtin.cpu.arch) {
 comptime {
     asm (archInfo.assembly);
 }
+
 extern fn ziro_stack_swap(current: *anyopaque, target: *anyopaque) void;
+
 pub const stackAlignment = 16;
-pub const CoroutineBase = struct {
+
+pub const CoroutineState = enum { Waiting, Finished, Running, Completed, WaitingForChannel };
+
+pub const CoroutineBase = packed struct {
     stack_pointer: [*]u8,
     const Func = *const fn (
         from: *CoroutineBase,
         self: *CoroutineBase,
     ) callconv(.c) noreturn;
+
+    pub const coroutineState: CoroutineState = .Running;
+
     pub fn init(func: Func, stack: []align(stackAlignment) u8) errors!CoroutineBase {
         if (@sizeOf(usize) != 8) @compileError("usize expected to take 8 bytes");
         if (@sizeOf(*Func) != 8) @compileError("function pointer expected to take 8 bytes");
@@ -60,9 +68,32 @@ pub const CoroutineBase = struct {
     pub inline fn resumeFrom(self: *CoroutineBase, from: *CoroutineBase) void {
         return ziro_stack_swap(from, self);
     }
-    pub fn yield(self: *@This(), target: *@This()) void {
+    pub fn yield(self: *@This(), target: *CoroutineBase) void {
         ziro_stack_swap(self, target);
         // When this returns, someone has resumed us
         // Execution continues from here with all registers restored
     }
 };
+
+//
+// this lib will provide(not this , I am talking about the lib as a whole), 2 fn go(Fn) and goRunCoro(Fn)- the go is like
+// the golang one while the goRunCoro will take in a fn and that fn should take in the argument Corotine type as I want to allow
+// the fn to pasue or resume later
+//
+// now the thing is that we have too keep track of the state so as we do not want to call the resume on it
+// 1) we can make the fn explicitly handle the state management
+// 2) we can take in the fn and then put it in another fn and when the fn(original) is over then we can in the end put the
+//  state to be done (enum)
+//
+//
+//  tuesday:7th Oct
+//  -- coroutine impl
+//  * implement coroBase : asm for both fn and also
+//  * the the yield mechanism , how to mark it's state to be ended when the
+//
+//  --- next is the scheduler --
+//
+//
+//
+
+////
--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,24 @@ const CoroutineBase = @import("./coroutine/coroutineBase.zig").CoroutineBase;
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us and the zig coro is {s}.\n", .{ "codebase", @typeName(ziro.asyncio) });
-    std.debug.print("the return value of the fn is {s}\n", .{"---"});
-    var stack: [81192]u8 align(16) = undefined;
+    std.debug.print("the  stack size of a() is {d} kb\n", .{@intFromPtr(&a) / 1000});
+    std.debug.print("the  stack size of main() is {d} kn\n", .{@intFromPtr(&main) / 1000});
+    b();
+    std.debug.print("the fn b finished\n", .{});
+    var stack: [1024 * 5]u8 align(16) = undefined;
     var worker = try CoroutineBase.init(a, &stack);
     var main_coro = CoroutineBase{ .stack_pointer = undefined };
     worker.resumeFrom(&main_coro);
     worker.resumeFrom(&main_coro);
+}
+
+pub fn b() void {
+    std.debug.print(" hi this statement is from fn b() and I am now going to yield\n", .{});
+    // var stack: [1024 * 5]u8 align(16) = undefined;
+    // var worker = try CoroutineBase.init(main, &stack);
+    var worker = CoroutineBase{ .stack_pointer = undefined };
+    worker.yield(&worker);
+    std.log.err("-------in b() this should not execute ------\n", .{});
 }
 pub fn a(from: *CoroutineBase, self: *CoroutineBase) callconv(.c) noreturn {
     std.debug.print("hi from the fn a() and now we are suspending it \n", .{});
@@ -23,7 +34,7 @@ pub fn a(from: *CoroutineBase, self: *CoroutineBase) callconv(.c) noreturn {
     std.debug.print("hi from the fn a() and now we have resumed it \n", .{});
 
     // Must yield back one final time (can't just return)
-    from.resumeFrom(self);
+    self.yield(from);
 
     unreachable; // Mark that we never actually return
 }
